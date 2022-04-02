4d61b6393f7764152900e579f5c4664ec37da5ae
João Vitor Nogueira <jvnogueira2010@gmail.com>
2022-04-02 02:58:04 -0300

    Projeto iniciado para automatizar entrada no AVA

---
+++ ScriptUNIVESP.js
@@ -0,0 +1,33 @@
+
+
+
+
+ if (document.querySelectorAll('.btn-red.btn-primary')[0] !== 'undefined') {
+   
+    setTimeout(() => {
+
+        document.querySelectorAll('.btn-red.btn-primary')[0].click();
+    },1000);
+
+    setTimeout(() => {
+   
+            document.querySelectorAll('.btn-red.btn-primary')[1].click();
+    
+        },5000);
+            
+    
+    
+    } 
+  
+
+    
+    
+    document.getElementById('login-sou').click();
+
+
+
+    
+
+
+
+    
