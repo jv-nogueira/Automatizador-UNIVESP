743dea41e97af91a444b009c7083d9676bbc5b34
João Vitor Nogueira <jvnogueira2010@gmail.com>
2022-04-07 09:27:17 -0300

    Atualizado o projeto para funcionar apenas no link mencionado

--- ScriptUNIVESP.js
+++ ScriptUNIVESP.js
@@ -2,6 +2,133 @@
 
 
 
+
+
+if(document.domain === 'ava.univesp.br'){
+	execBtnTrigger();
+
+		function execBtnTrigger(){
+            setTimeout(function(){
+	
+						if(typeof (document.getElementById('login-sou') !== 'undefined')){    
+   
+                                document.getElementById('login-sou').click();
+             
+						} 
+                    }, 2000);
+					
+                }
+                execBtnTrigger();
+
+            }
+
+            if(document.domain === 'login.univesp.br'){
+                execBtnTrigger2();
+            
+                    function execBtnTrigger2(){
+                        setTimeout(function(){
+                   
+                            if(typeof (document.querySelectorAll('.btn-red.btn-primary')[0] !== 'undefined')){
+                               
+                                document.querySelectorAll('.btn-red.btn-primary')[0].click();
+                        
+                            }
+
+                            
+                        }, 5000);
+                                
+
+                            }
+                            execBtnTrigger2();
+            
+                        }
+
+                        if(document.domain === 'login.univesp.br'){
+                            execBtnTrigger3();
+                        
+                                function execBtnTrigger3(){
+                                    setTimeout(function(){
+                               
+                                        if(typeof (document.querySelectorAll('.btn-red.btn-primary')[1] !== 'undefined')){
+                                           
+                                            document.querySelectorAll('.btn-red.btn-primary')[1].click();
+                                    
+                                        }
+                                         
+            
+                                        
+                                    }, 5000);
+                                            
+                        
+                        
+                        
+                                        }
+                                        execBtnTrigger3();
+                        
+                                    }
+
+                       
+
+
+
+/*
+
+
+
+                        setTimeout(function(){
+                   
+                                        if(typeof (document.querySelectorAll('.btn-red.btn-primary')[0] !== 'undefined')){
+                                           
+                                            document.querySelectorAll('.btn-red.btn-primary')[0].click();
+                                    
+                                        }
+                                    }, 5000);
+                                   
+                                        if(typeof (document.querySelectorAll('.btn-red.btn-primary')[1] !== 'undefined')){
+                                            setTimeout(function(){
+                                            document.querySelectorAll('.btn-red.btn-primary')[1].click();
+                                        }, 5000);
+                                        }
+                               
+
+
+                                        
+                                    
+                    
+                               
+                            
+				
+
+
+
+var button = document.getElementById('login-sou').click();
+
+var i = 0;
+    
+button.addEventListener('click', function click(e) {
+    i++;
+    console.log("Essa função foi chamada %d vez", i);
+    button.removeEventListener('click', click);
+});
+
+
+ else if(execBtnTrigger == true) {
+    execBtnTrigger2();
+
+    function execBtnTrigger2(){
+            setTimeout(function(){
+
+                    if(typeof (document.querySelectorAll('.btn-red.btn-primary')[0] !== 'undefined')){
+                        document.querySelectorAll('.btn-red.btn-primary')[0].click();
+                    }
+                    execBtnTrigger2();
+
+            }, 2500);
+        }
+    
+}else 
+
+
  if (document.querySelectorAll('.btn-red.btn-primary')[0] !== 'undefined') {
    
     setTimeout(() => {
@@ -19,15 +146,12 @@
     
     } 
   
-
-    
     
     document.getElementById('login-sou').click();
 
-
-
-    
-
-
+*/
 
     
+
+
+
